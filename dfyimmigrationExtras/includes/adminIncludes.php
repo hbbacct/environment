e54ea69846e556f336f0f4dcf7ddc4ecde91b05b
hbbact <ryan@hbbacct.com>
2023-05-26 02:25:07 +0800

    cleanup files

--- dfyimmigrationExtras/includes/adminIncludes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('dfyimmigrationExtras/class/utility.php');
-$db = Utility::getDBConnection();
-session_start();
-
-?>
--- dfytaxExtras/includes/adminIncludes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('dfytaxExtras/class/utility.php');
-$db = Utility::getDBConnection();
-session_start();
-
-?>
